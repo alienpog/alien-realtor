7dfd4e8c320fc42da5de1a9ad56140f6c05da8d4
alienpogtech <110186979+alienpogtech@users.noreply.github.com>
2024-06-05 17:01:03 +0100

    alien realtor 10

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -1,6 +1,7 @@
 import { MapPinIcon } from "@heroicons/react/24/solid"
 import { PhoneArrowDownLeftIcon } from "@heroicons/react/24/solid"
 import Image from "next/image"
+import Link from "next/link"
 
 function Contact() {
   return (
@@ -11,18 +12,18 @@ function Contact() {
                 <PhoneArrowDownLeftIcon className="w-4 h-4 text-[#CE0000] animate-ping"/>
                 <p className="tracking-widest">09068801955</p>
             </div>
-            <div className="max-w-[220px] flex justify-start items-center space-x-3 text-[#2E2E2E] text-base font-semibold">
+            <Link href="https://www.tiktok.com/@alienrealtor?_t=8mwwrFkOW4D&_r=1" className="max-w-[220px] flex justify-start items-center space-x-3 text-[#2E2E2E] text-base font-semibold">
                 <Image src= "/assets/IMG/Icons/Frame.png" width={96} height={96} alt="icon" className="w-4 h-4 text-[#CE0000]"/>
                 <p>alienrealtor</p>
-            </div>
+            </Link>
             <div className="max-w-[230px] flex justify-start items-center space-x-3 text-[#2E2E2E] text-base font-semibold">
             <Image src= "/assets/IMG/Icons/Frame2.png" width={96} height={96} alt="icon" className="w-4 h-4 text-[#9D5524]"/>
                 <p>alienrealtormail@gmail.com</p>
             </div>
-            <div className="max-w-[230px] flex justify-start items-center space-x-3 text-[#2E2E2E] text-base font-semibold">
+            <Link href="https://www.instagram.com/alienrealtor?igsh=cWxmZHcxem16cGh6&utm_source=qr" className="max-w-[230px] flex justify-start items-center space-x-3 text-[#2E2E2E] text-base font-semibold">
                 <Image src= "/assets/IMG/Icons/Frame3.png" width={96} height={96} alt="icon" className="w-4 h-4 text-[#9D5524]"/>
                 <p>alienrealtor</p>
-            </div>
+            </Link>
         </div>
         <div className="flex items-center justify-start space-x-3 text-[#9D5524] text-base font-semibold">
             <MapPinIcon className="min-w-4 h-4 text-[#9D5524]"/>
